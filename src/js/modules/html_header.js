d4d718ccf9da5269f14ea2c14620b6b9a4f2b9de
Pere Esteve <pereesteve@hitabook.local>
2020-05-11 11:24:51 +0200

    Linted html_header.js

--- src/js/modules/html_header.js
+++ src/js/modules/html_header.js
@@ -4,14 +4,14 @@ class HtmlHeader {
     let vm = this
     let menuMobile = document.querySelector('.html-header__menu--mobile')
     let menu = document.querySelector('.html-header__menu')
-    menuMobile.addEventListener('click', function() {
+    menuMobile.addEventListener('click', function () {
       vm.toggleMenu(menu)
     }, false)
     let menuItems = document.querySelectorAll('.menu-item a')
     Array.from(menuItems).forEach(link => {
-        link.addEventListener('click', function(event) {
-          vm.toggleMenu(menu)
-        })
+      link.addEventListener('click', function (event) {
+        vm.toggleMenu(menu)
+      })
     })
   }
   reset () {
@@ -19,9 +19,9 @@ class HtmlHeader {
   }
   toggleMenu (menu) {
     if (menu.classList.contains('active')) {
-      menu.classList.remove('active');
+      menu.classList.remove('active')
     } else {
-      menu.classList.add('active');
+      menu.classList.add('active')
     }
   }
 }
